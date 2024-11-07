17bc3eb34bfa813621b64763714be36ee60ea8bb
Cody Headings <codyheadings05@gmail.com>
2024-11-06 21:55:37 -0500

    Finished battle_model tests and corrected a docstring in battle_model

--- meal_max/tests/test_battle_model.py
+++ meal_max/tests/test_battle_model.py
@@ -9,11 +9,6 @@ def battle_model():
     """Fixture to provide a new instance of BattleModel for each test."""
     return BattleModel()
 
-#@pytest.fixture
-#def mock_update_play_count(mocker):
-    #"""Mock the update_play_count function for testing purposes."""
-    #return mocker.patch("music_collection.models.playlist_model.update_play_count")
-
 """Fixtures providing sample meals for the tests."""
 @pytest.fixture
 def sample_meal1():
@@ -24,8 +19,86 @@ def sample_meal2():
     return Meal(2, 'Meal 2', 'Cuisine 2', 3.5, 'HIGH')
 
 @pytest.fixture
+def sample_meal3():
+    return Meal(3, 'Meal 3', 'Cuisine 3', 7, 'MED')
+
+@pytest.fixture
 def sample_battle(sample_meal1, sample_meal2):
     return [sample_meal1, sample_meal2]
 
-# need tests for battle(), clear_combatants(), get_battle_score(),
-# get_combatants(), and prep_coombatants
+##################################################
+# Battle Combatants Test Cases
+##################################################
+
+def test_too_few_combatants(battle_model, sample_battle):
+    """Test error when too few meals are in a battle list of combatants."""
+    battle_model.combatants.extend(sample_battle)
+
+    with pytest.raises(ValueError, match="Two combatants must be prepped for a battle."):
+            battle_model.battle()
+
+def test_battle(battle_model, sample_battle):
+     """Test that battle function successfully returns a winning meal"""
+     battle_model.combatants.extend(sample_battle)
+
+     winner = battle_model.battle()
+     assert type(winner) == Meal
+
+##################################################
+# Remove Combatants Test Cases
+##################################################
+
+def test_clear_combatants(battle_model, sample_battle):
+    """Test clearing the combatants list."""
+    battle_model.combatants.extend(sample_battle)
+
+    battle_model.clear_combatants()
+    assert len(battle_model.playlist) == 0, "Combatants list should be empty after clearing"
+
+##################################################
+# Combatants Score Test Cases
+##################################################
+
+def test_get_battle_score(battle_model, sample_battle):
+     """Test that battle score function returns the correct score"""
+     battle_model.combatants.extend(sample_battle)
+
+     score = battle_model.get_battle_score()
+     assert score == 105 # (12 * 9) - 3 for meal 1
+
+##################################################
+# Combatants Retrieval Test Cases
+##################################################
+
+def test_get_combatants(battle_model, sample_battle):
+    """Test successfully retrieving all comabtants from the battle."""
+    battle_model.combatants.extend(sample_battle)
+
+    all_combatants = battle_model.get_combatants()
+    assert len(all_combatants) == 2
+    assert all_combatants[0].id == 1
+    assert all_combatants[1].id == 2
+
+##################################################
+# Prep Combatants Test Cases
+##################################################
+
+def test_too_many_combatants(battle_model, sample_battle, sample_meal3):
+    """Test error from adding a meal to a full battle list."""
+    battle_model.combatants.extend(sample_battle)
+    with pytest.raises(ValueError, match="Combatant list is full, cannot add more combatants."):
+            battle_model.prep_combatants(sample_meal3)
+
+def test_prep_one_combatant(battle_model, sample_meal1):
+    """Test successfully adding a meal to the combatant list of a battle."""
+    battle_model.prep_combatants(sample_meal1)
+    assert len(battle_model.combatants) == 1
+    assert battle_model.combatants[0].id == 1
+
+def test_prep_two_combatants(battle_model, sample_meal1, sample_meal2):
+    """Test successfully adding two meals to the combatant list of a battle."""
+    battle_model.prep_combatants(sample_meal1)
+    battle_model.prep_combatants(sample_meal2)
+    assert len(battle_model.combatants) == 2
+    assert battle_model.combatants[0].id == 1
+    assert battle_model.combatants[1].id == 2
